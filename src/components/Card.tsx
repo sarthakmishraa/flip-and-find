309ab9102a9dadc1236b28aea932141fc5f62ac6
Sarthak Mishra <msarthak785@gmail.com>
2024-06-18 16:55:20 -0400

    added flip animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,7 +22,7 @@ export const Card: React.FC<CardProps> = ({ image, handleChoice, turnedUp, disab
     };
 
     return(
-        <div className="relative w-[80px] lg:w-[150px] xl:w-[200px] cursor-pointer shadow-4xl" >
+        <div className="relative w-[80px] lg:w-[150px] xl:w-[200px] cursor-pointer shadow-4xl active:rotate-180 active:transition-all" >
             <div>
                 {
                     turnedUp ? (
